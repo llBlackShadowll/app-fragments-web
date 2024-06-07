0b34704049d354bcef2406e4741346f49df65fa4
llBlackShadowll <jvelitquintana2@gmail.com>
2024-06-06 21:58:45 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/app_fragments_web/AppFragmentsWebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fragments_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFragmentsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFragmentsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_fragments_web/AppFragmentsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fragments_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFragmentsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
